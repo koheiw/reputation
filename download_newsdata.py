b45d9e81965a0a09d6cb57772258380e00227e30
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-04-17 08:00:18 +0900

    Save download log

--- download_newsdata.py
+++ download_newsdata.py
@@ -4,17 +4,40 @@ import configparser
 from pymongo import MongoClient, errors
 from newsdataapi import NewsDataApiClient
 
-def download(date, col):
+def check_log(url, date, col):
+  
+  date = (datetime.datetime.strptime(date[0], "%Y-%m-%d"),
+          datetime.datetime.strptime(date[1], "%Y-%m-%d"))
+  res = col.find_one(
+      {"url": url, "date": date, "complete": True}
+  )
+  return res != None
+  
+def save_log(url, date, total, col):
+  
+  col.create_index(["url", "date"], unique = True)
+  
+  date = (datetime.datetime.strptime(date[0], "%Y-%m-%d"),
+          datetime.datetime.strptime(date[1], "%Y-%m-%d"))
+          
+  col.update_one(
+      {"url": url, "date": date}, 
+      {"$set": {"complete": date[1] < datetime.date.today(), 
+                "total": total,
+                "timestamp": datetime.datetime.now()}}, 
+       upsert = True
+  )
+
+def download(url, date, col):
     
     col.create_index("article_id", unique = True)
-    
     api = NewsDataApiClient(apikey = config['newsdata']['key'])
     
     page = None
     last = 0
     while True:
-        data = api.archive_api(domainurl = "inquirer.net", page = page, sort = "pubdateasc",
-                               from_date = date[0].isoformat()[0:10], to_date = date[1].isoformat()[0:10])
+        data = api.archive_api(domainurl = url, page = page, sort = "pubdateasc",
+                               from_date = date[0], to_date = date[1])
         total = data['totalResults']
         articles = data['results']
         inserted = 0;
@@ -30,11 +53,12 @@ def download(date, col):
             
         last += len(articles)     
         print(f"{last}/{total}: {inserted} inserted")
-        time.sleep(5)
         page = data['nextPage']
         if not page:
             break
-    return 
+        time.sleep(1)
+        
+    return total
 
 if __name__ == "__main__":
     
@@ -43,13 +67,19 @@ if __name__ == "__main__":
     
     con = MongoClient('192.168.10.101', 27017)
     db = con.reputation
-
+    
+    url = "inquirer.net"
     cal = calendar.Calendar()
-    for year in range(2025, 2027):
-        for month in range(1, 13):
+    for year in range(2024, 2025):
+        for month in range(12, 13):
             date = (datetime.datetime(year, month, 1), 
                     datetime.datetime(year, month, max([d for d in cal.itermonthdays(year, month)])))
-            print(f"{date[0].isoformat()[0:10]} to {date[1].isoformat()[0:10]}")
-            download(date, db.newsdata)
+            date = (date[0].isoformat()[0:10], date[1].isoformat()[0:10])
+            if (check_log(url, date, db.log)):
+              print(f"Skip {date[0]} to {date[1]}")
+              continue
+            print(f"Download {date[0]} to {date[1]}")  
+            total = download(url, date, db.newsdata)
+            save_log(url, date, total, db.log)
 
     con.close()
